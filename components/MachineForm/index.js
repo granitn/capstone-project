006c5f82841525037f33bfb0ad50cf0ebad4515b
Granit Neziraj <granit.n@outlook.de>
2023-05-09 10:29:23 +0200

    form working adds new to database

--- components/MachineForm/index.js
+++ components/MachineForm/index.js
@@ -19,10 +19,11 @@ export default function MachineForm() {
 
     const formData = new FormData(event.target);
     const machineAddData = Object.fromEntries(formData);
-    console;
-    await trigger(machineAddData);
 
-    event.target.reset();
+    await trigger(machineAddData);
+    console.log(machineAddData);
+
+    //event.target.reset();
   }
 
   return (
@@ -30,15 +31,28 @@ export default function MachineForm() {
       <h3>Add a new machine</h3>
       <label htmlFor="name">
         Machine Name:
-        <input type="text" name="machineName" id="name" />
+        <input type="text" name="machineName" id="name" required />
       </label>
       <label htmlFor="settings">
         Machine Settings:
-        <textarea type="text" name="settings" id="settings" />
+        <textarea
+          type="text"
+          name="settings"
+          id="settings"
+          rows="4"
+          placeholder="seperate settings with linebreaks/enter"
+          required
+        />
       </label>
       <label htmlFor="image">
         Machine Picture:
-        <input type="text" name="machineImage" id="image" />
+        <input
+          type="url"
+          name="machineImage"
+          id="image"
+          placeholder="image url from unsplash"
+          pattern="https://images.unsplash.com/.*"
+        />
       </label>
       <button type="submit">Submit</button>
     </form>
